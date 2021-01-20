f4746572b5559996218f9e6247adf355df441140
Alan Ramirez <43681186+SplitCoder@users.noreply.github.com>
2021-01-20 18:23:25 -0500

    Step 1 Almost done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,19 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-  return <div className="app">Trivia!</div>;
+QuestionNum = 0
+  return <div className="app"> <Question Q= {data[0].}/> </div>;
+
+ 
 }
 
 export default App;
+
+function Question (props){
+  return(
+  <div classname=
+  "Questions">
+    <div className="app"> {props.Q}  </div>;
+  </div>
+  );
+}
