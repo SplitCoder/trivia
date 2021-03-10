39d993fe7921747c153bfcdeb6a4525db87adc13
Alan Ramirez <43681186+SplitCoder@users.noreply.github.com>
2021-03-10 18:18:15 -0500

    Day 3 Goal 1 completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,8 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-// let [ questionNum, SetquestionNum ] = useState
-  QuestionNum = data[0]
+let [ questionNum, SetquestionNum ] = useState(0)
+  //QuestionNum = data[0]
 let [ correctAnswer, SetcorrectAnswer ] = useState(0);
 let [ isAnswered, SetisAnswered ] = useState(false);
 
@@ -12,13 +12,15 @@ if (isAnswered == false) {
   CorrectAnswer = "Question is not answered"
 
 } else {
-CorrectAnswer = "The Correct Answer is " + data[0].question.correct_choice_index
+CorrectAnswer = "The Correct Answer is " + data[questionNum].question.correct_choice_index
 
 }
 
 
-  return <div className="app"> <Question Q= {data[0].question.text}/> 
-  <Nextquestion/> <br/><button onClick={() => SetisAnswered(true)}>Click for the correcty answer </button> <br/>{CorrectAnswer}</div>;
+  return <div className="app"> <Question Q= {data[questionNum].question.text} QN= {questionNum}/>
+  <Nextquestion QN= {questionNum} QNN= {SetquestionNum} NQ={SetisAnswered}/> 
+  <br/>
+  <button onClick={() => SetisAnswered(true)}>Click for the correcty answer </button> <br/>{CorrectAnswer}</div>;
   
  
 }
@@ -29,11 +31,11 @@ function Question (props){
   return(
   <div classname=
   "Questions">
-    <div className="app"> {props.Q}  </div>
+    <div className="app"> {props.Q} </div>
     
 {
 
-  data[0].question.choices.map((choice)=> 
+  data[props.QN].question.choices.map((choice)=> 
   {return <Answer A={choice}/>})
 }
 
@@ -41,10 +43,14 @@ function Question (props){
   );
 }
 
-function Nextquestion (){
-  return(
-    <button>Next Question</button>
-
+function Nextquestion (props){
+  return(<div className="app">
+    <button onClick={() => {
+      props.QNN( props.QN + 1) 
+      props.NQ( false ) } 
+      }>Next Question</button>
+    
+</div>
 
     
   );
